3792cff4dbfaa96f0c8a04bf99603e1a26311ef6
scottblackdb <scottblackdb>
2025-01-24 21:46:20 +0000

    Finished Initial Drafts of Labs 1 & 2

---
+++ Lab 1 - Data Ingestion/Lab 1.py
@@ -0,0 +1,58 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Data Engineering Lab 1: Building a Modern Data Pipeline with Databricks Medallion Architecture
+# MAGIC
+# MAGIC ## Overview
+# MAGIC
+# MAGIC In this hands-on workshop, you'll learn how to implement a robust data pipeline using Databricks' medallion architecture - a multi-layered data organization framework that helps ensure data quality, reliability, and usability at scale. The medallion architecture, also known as "multi-hop" architecture, consists of bronze (raw), silver (validated), and gold (enriched) layers that progressively refine and transform data.
+# MAGIC
+# MAGIC ## Workshop Objectives
+# MAGIC
+# MAGIC By the end of this lab, you will:
+# MAGIC
+# MAGIC * Understand the core principles and benefits of the medallion architecture
+# MAGIC * Build data pipelines that move data through bronze, silver, and gold layers
+# MAGIC * Implement data transformation
+# MAGIC * Create optimized tables for downstream analytics and ML workloads
+# MAGIC * Apply best practices for performance and maintainability
+# MAGIC
+# MAGIC ## Prerequisites
+# MAGIC
+# MAGIC To get the most out of this workshop, you should have:
+# MAGIC
+# MAGIC * Basic familiarity with SQL and Python
+# MAGIC * Understanding of basic data engineering concepts
+# MAGIC * Access to a Databricks workspace
+# MAGIC * Basic knowledge of Delta Lake operations
+# MAGIC
+# MAGIC ## Environment
+# MAGIC
+# MAGIC * Ingest data from three sources, SQL Server, JSON files and Rest API into bronze layer
+# MAGIC * Transform data to create silver layer
+# MAGIC * Create aggregate data source in the gold layer
+# MAGIC * All labs will leverage serverless compute
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
+# MAGIC ## Getting Started
+# MAGIC In a separate browser or browser tab, go to your Databricks workspace for 
+# MAGIC * Data Ingestion/Bronze Ingest Medical Providers Locations
+# MAGIC
+# MAGIC Then complete the notebooks in this order
+# MAGIC * Bronze Ingest Provider Reviews
+# MAGIC * Bronze Ingest TMSIS
+# MAGIC
+# MAGIC ####With the bronze layer built next build the silver layer
+# MAGIC * Silver Clean Providers
+# MAGIC * Silver Clean TMSIS
+# MAGIC
+# MAGIC ####Finally build the gold layer
+# MAGIC * Gold - TMSIS Monthtly Summary
+# MAGIC * Gold Aggregate Provider Claims
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## End of Lab 1
