4f23add4bad9b486e15eecd4a57661fdd5c7a4c3
scottblackdb <scottblackdb>
2025-01-31 21:04:52 +0000

    Set a location when creating a catalog

--- Lab 0 - Setup/Create User Catalog.py
+++ Lab 0 - Setup/Create User Catalog.py
@@ -10,8 +10,19 @@
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC
+# MAGIC ####By default the first external location will be used when creating the catalog. To override at a new line to the cell below and set the path variable to the URL where to create the catalog
 
-spark.sql(f"CREATE CATALOG IF NOT EXISTS {ctlg}")
+# COMMAND ----------
+
+df = spark.sql("show external locations").limit(1).toPandas()
+path = df['url'][0]
+
+# COMMAND ----------
+
+
+spark.sql(f"CREATE CATALOG IF NOT EXISTS {ctlg} MANAGED LOCATION '{path}'")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {ctlg}.bronze")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {ctlg}.silver")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {ctlg}.gold")
