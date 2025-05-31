d146c4382f6d121078cca72b3ea0057221cb0ed7
scottblackdb <scottblackdb>
2025-05-31 06:46:36 +0000

    Added an optional R lab

--- Lab 0 - Setup/Create User Catalog.py
+++ Lab 0 - Setup/Create User Catalog.py
@@ -12,17 +12,25 @@
 
 # MAGIC %md
 # MAGIC
-# MAGIC ####By default the first external location will be used when creating the catalog. To override at a new line to the cell below and set the path variable to the URL where to create the catalog
+# MAGIC By default the first external location will be used when creating the catalog. To override at a new line to the cell below and set the path variable to the URL where to create the catalog
 
 # COMMAND ----------
 
 df = spark.sql("show external locations").limit(1).toPandas()
-path = df['url'][0]
+path = df['url'].iloc[0] if not df.empty else None
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC If there are no external locations the default location for the catalog will be used.
 
-spark.sql(f"CREATE CATALOG IF NOT EXISTS {ctlg} MANAGED LOCATION '{path}'")
+# COMMAND ----------
+
+if path is None:
+    spark.sql(f"CREATE CATALOG IF NOT EXISTS {ctlg}")
+else:
+    spark.sql(f"CREATE CATALOG IF NOT EXISTS {ctlg} MANAGED LOCATION '{path}'")
+    
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {ctlg}.bronze")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {ctlg}.silver")
 spark.sql(f"CREATE SCHEMA IF NOT EXISTS {ctlg}.gold")
